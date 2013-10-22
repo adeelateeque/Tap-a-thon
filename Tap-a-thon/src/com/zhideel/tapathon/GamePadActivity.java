7f2d7ae3298995394df9c105dd1d7dbfbb2091d1
hzhiguang <rune199@hotmail.com>
2013-10-22 18:00:04 +0800

    Commited

--- Tap-a-thon/src/com/zhideel/tapathon/GamePadActivity.java
+++ Tap-a-thon/src/com/zhideel/tapathon/GamePadActivity.java
@@ -1,79 +1,118 @@
 package com.zhideel.tapathon;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 //TODO add double tap listener
 //TODO add long tap listener
 public class GamePadActivity extends Activity {
-    private boolean continueMusic;
-    private GridView gridview;
+	private boolean continueMusic;
+	private GridView gridview;
+	private ArrayList<Integer> operands;
+	private String operator;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_game_pad);
-        gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new PadAdapter(this));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_game_pad);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(GamePadActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+		operands = new ArrayList<Integer>();
 
-    protected void onPause() {
-        super.onPause();
-        if (!continueMusic) {
-            MusicManager.pause();
-        }
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        continueMusic = false;
-        MusicManager.start(this, MusicManager.MUSIC_MENU);
-    }
+		gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new PadAdapter(this));
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+	}
+
+	protected void onPause() {
+		super.onPause();
+		if (!continueMusic) {
+			MusicManager.pause();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		continueMusic = false;
+		MusicManager.start(this, MusicManager.MUSIC_MENU);
+	}
+
+	public void addOperand(Integer operand) {
+		if (operands.size() < 2) {
+			operands.add(operand);
+		} else {
+			if (operator != null){
+				doCalc();
+			}
+		}
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+		if (operands.size() == 2) { doCalc(); }
+	}
+
+	private int doCalc() {
+		Integer op1 = operands.get(0);
+		Integer op2 = operands.get(1);
+		Integer result = 0;
+		if (operator.equalsIgnoreCase("X")) {
+			result = op1 * op2;
+		} else if (operator.equalsIgnoreCase("/")) {
+			result = op1 / op2;
+		} else if (operator.equalsIgnoreCase("-")) {
+			result = op1 - op2;
+		} else {
+			result = op1 + op2;
+		}
+		Toast.makeText(this, Integer.toString(result), Toast.LENGTH_LONG).show();
+		return result;
+	}
+
 }
 
 class PadAdapter extends BaseAdapter {
-    private Context mContext;
-    private ArrayList<MultiTouchView> tappads;
-    public PadAdapter(Context c) {
-        mContext = c;
-        tappads  = new ArrayList<MultiTouchView>();
-    }
+	private Context mContext;
+	private ArrayList<MultiTouchView> tappads;
 
-    public int getCount() {
-        return 9;
-    }
+	public PadAdapter(Context c) {
+		mContext = c;
+		tappads = new ArrayList<MultiTouchView>();
+	}
 
-    public Object getItem(int position) {
-        return tappads.get(position);
-    }
+	public int getCount() {
+		return 9;
+	}
 
-    public long getItemId(int position) {
-        return 0;
-    }
+	public Object getItem(int position) {
+		return tappads.get(position);
+	}
 
-    public View getView(int position, View convertView, ViewGroup parent) {
-        MultiTouchView tappad;
-        tappad = new MultiTouchView(mContext, null);
-        tappad.setLayoutParams(new GridView.LayoutParams(parent.getWidth()/3, parent.getHeight()/3-7));
-        tappad.setAlpha(0.4f);
-        return tappad;
-    }
+	public long getItemId(int position) {
+		return 0;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		MultiTouchView tappad;
+		tappad = new MultiTouchView(mContext, null);
+		tappad.setLayoutParams(new GridView.LayoutParams(parent.getWidth() / 3,
+				parent.getHeight() / 3 - 7));
+		tappad.setAlpha(0.4f);
+		return tappad;
+	}
 
 }
--- Tap-a-thon/src/com/zhideel/tapathon/MultitouchView.java
+++ Tap-a-thon/src/com/zhideel/tapathon/MultitouchView.java
@@ -1,25 +1,32 @@
 package com.zhideel.tapathon;
 
+import java.util.Random;
+
 import android.content.Context;
-import android.graphics.*;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.SparseArray;
 import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.Random;
+import android.widget.Toast;
 
 public class MultiTouchView extends View {
-
+	// Usually this can be a field rather than a method variable
+    private static Random rand = new Random();
     private static final int SIZE = 60;
     private SparseArray<PointF> mActivePointers;
     private Paint mPaint;
-    private int[] colors = {Color.BLUE, Color.GREEN, Color.MAGENTA,
-            Color.BLACK, Color.CYAN, Color.GRAY, Color.RED, Color.DKGRAY,
-            Color.LTGRAY, Color.YELLOW};
-
+    private int[] colors = {Color.BLUE, Color.GREEN, Color.MAGENTA, Color.CYAN, Color.RED, Color.YELLOW};
+    private boolean isSelected;
     private Paint textPaint;
+    private String currentText;
 
 
     public MultiTouchView(Context context, AttributeSet attrs) {
@@ -34,44 +41,68 @@ public class MultiTouchView extends View {
         mPaint.setColor(Color.WHITE);
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(Color.WHITE);
+        textPaint.setShadowLayer(5.0f, 5.0f, 5.0f, Color.BLACK);
         textPaint.setTextSize(100);
 
         randomPaint();
+        randText();
     }
 
-    private void randomPaint()
-    {
+    private void randomPaint() {
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                MultiTouchView.this.setBackgroundColor(colors[randInt(0, 3)]);
+                if(!isSelected){
+                	MultiTouchView.this.setBackgroundColor(colors[randInt(0, 5)]);
+                }
+                else
+                {
+                	MultiTouchView.this.setBackground(MultiTouchView.this.getResources().getDrawable(R.drawable.fire_alert));
+                }
                 invalidate();
                 randomPaint();
+                randText();
             }
         }, getRandomDelay());
     }
 
-    private void randomShock()
-    {
+    private void randomShock() {
         Bitmap fireAlert = BitmapFactory.decodeResource(getResources(),
                 R.drawable.fire_alert);
     }
 
-    private int getRandomDelay()
-    {
+    private int getRandomDelay() {
        return randInt(1, randInt(1, 3)) * randInt(0, 1) < 0.5 ? 500 : 1000;
     }
 
 
     public int randInt(int min, int max) {
-
-        // Usually this can be a field rather than a method variable
-        Random rand = new Random();
-
         // nextInt is normally exclusive of the top value,
         // so add 1 to make it inclusive
         int randomNum = rand.nextInt((max - min) + 1) + min;
 
         return randomNum;
+    }
+    
+    private void randText() {
+    	if(!isSelected) {
+    		int rand = randInt(0, 13);
+    		if (rand == 10){
+    			currentText = "+";
+    		}
+    		else if (rand == 11){
+    			currentText = "-";
+    		}
+    		else if (rand == 12){
+    			currentText = "X";
+    		}
+    		else if (rand == 13){
+    			currentText = "/";
+    		}
+    		else {
+    			currentText = Integer.toString(rand);
+    		}
+    	}
     }
 
     @Override
@@ -90,12 +121,20 @@ public class MultiTouchView extends View {
 
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN: {
+            	this.isSelected = true;
+            	try{
+            		((GamePadActivity) super.getContext()).addOperand(Integer.parseInt(currentText));
+            	}catch(NumberFormatException e)
+            	{
+            		((GamePadActivity) super.getContext()).setOperator(currentText);
+            	}
                 // We have a new pointer. Lets add it to the list of pointers
-
+            	
                 PointF f = new PointF();
                 f.x = event.getX(pointerIndex);
                 f.y = event.getY(pointerIndex);
                 mActivePointers.put(pointerId, f);
+                Toast.makeText(getContext(), currentText, Toast.LENGTH_SHORT).show();
                 break;
             }
             case MotionEvent.ACTION_MOVE: { // a pointer was moved
@@ -139,7 +178,7 @@ public class MultiTouchView extends View {
 
             canvas.drawBitmap(getResizedBitmap(fireAlert, canvas.getHeight(), canvas.getWidth()), 0 , 0, null);
         }
-        canvas.drawText("X", this.getWidth()/2 - 50, this.getHeight()/2, textPaint);
+       canvas.drawText(currentText, this.getWidth()/2 - 20, this.getHeight()/2 + 40, textPaint);
     }
 
     public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
