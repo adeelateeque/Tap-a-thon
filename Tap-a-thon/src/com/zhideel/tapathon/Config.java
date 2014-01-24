d8c1ed9d4fd6e319b8a9cf27fc9ba809e65a6332
Adeel <adeelateeque@gmail.com>
2014-01-24 15:48:41 +0800

    Added logic to mirror all screens and various other fixes

---
+++ Tap-a-thon/src/com/zhideel/tapathon/Config.java
@@ -0,0 +1,10 @@
+package com.zhideel.tapathon;
+
+import com.zhideel.tapathon.App;
+
+/**
+ * Created by Adeel on 24/1/14.
+ */
+public final class Config {
+    public static App context = null;
+}
